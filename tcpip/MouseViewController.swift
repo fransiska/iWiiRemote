5cb8658505ae156f89e5dae67747cc76ade794f9
fransiska <fransiska.basoeki@gmail.com>
2017-06-11 12:00:21 +0700

    add scroll view and detect swipes

--- tcpip/MouseViewController.swift
+++ tcpip/MouseViewController.swift
@@ -15,12 +15,21 @@ class MouseViewController: UIViewController {
 
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var swipeView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let directions: [UISwipeGestureRecognizerDirection] = [.right, .left, .up, .down]
+        for direction in directions {
+            let gesture = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+            gesture.direction = direction
+            self.swipeView.addGestureRecognizer(gesture)
+        }
+
         updateButtonState()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -29,13 +38,29 @@ class MouseViewController: UIViewController {
     func updateButtonState() {
         leftButton.isEnabled = (sock>0)
         rightButton.isEnabled = (sock>0)
+        swipeView.isUserInteractionEnabled = (sock>0)
     }
     
     @IBAction func mouseButtonClicked(_ sender: UIButton) {
         if (sender === leftButton) {
-            tcpipSocket_send(sock, "left;")
+            tcpipSocket_send(sock, "leftclick;")
         } else if (sender === rightButton) {
+            tcpipSocket_send(sock, "rightclick;")
+        }
+    }
+    
+    func onSwipe(_ gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case UISwipeGestureRecognizerDirection.right:
             tcpipSocket_send(sock, "right;")
+        case UISwipeGestureRecognizerDirection.left:
+            tcpipSocket_send(sock, "left;")
+        case UISwipeGestureRecognizerDirection.up:
+            tcpipSocket_send(sock, "up;")
+        case UISwipeGestureRecognizerDirection.down:
+            tcpipSocket_send(sock, "down;")
+        default:
+            break
         }
     }
 }
